69f1494ee98002b2c21032dd0a1a26b027a198ac
barath-pt5690 <barath-pt5690@barath-pt5690.csez.zohocorpin.com>
2022-09-10 14:22:08 +0530

    Comments added

--- BasicClass.py
+++ BasicClass.py
@@ -1,9 +1,13 @@
 class College:
+    #Class attribute
     CLG = "BIT"
+
+    #Constructor (Parameterized)
     def __init__(self,name,rollno):
         self.name = name
         self.rollno = rollno
 
+    #Methods
     def showdetails(self):
         print(f"Name: {self.name} Roll No: {self.rollno} College: {self.CLG}")
 
@@ -20,14 +24,17 @@ class College:
 
 
 
+#First student instance
 student_1 = College("Barath","191CS138")
 
 student_1.showdetails()
 
+#changing the student name instance atrribute
 student_1.updatename("Barathkumar")
 
 student_1.showdetails()
 
+#Passing the dictionary
 student_1.setmarks({"English":90,"Tamil":100,"Mathematics":80})
 
 student_1.showmarks()
